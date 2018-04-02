d9fb95f0fccbd7372cd109ac773586459f60f5a4
Hung Dao <hungnt.dao@gmail.com>
2018-04-02 13:14:06 +0300

    fix login.php

--- login.php
+++ login.php
@@ -71,136 +71,93 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				} );
 	</script>
 </head>
+
 <?php 
 // Include config file
 require_once 'database/connection.php';
+
+// Define variables and initialize with empty values
+$email = $password = "";
+$email_err = $password_err = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 	
 	// Check if email is empty
-	
 	if(empty(trim($_POST["email"]))){
-		
 		$email_err = 'Please enter your email.';
-		
 	} else{
-		
 		$email = trim($_POST["email"]);
-		
 	}
 	
 	// Check if password is empty
-	
 	if(empty(trim($_POST['password']))){
-		
 		$password_err = 'Please enter your password.';
-		
 	} else{
-		
 		$password = trim($_POST['password']);
-		
 	}
 	
 	// Validate credentials
-	
 	if(empty($email_err) && empty($password_err)){
 		
 		// Prepare a select statement
-		
 		$sql = "SELECT email, password FROM user WHERE email = ?";
-		
-		
 		
 		if($stmt = mysqli_prepare($link, $sql)){
 			
-			// Bind variables to the prepared statement as parameters
-			
+			// Bind variables to the prepared statement as parameters	
 			mysqli_stmt_bind_param($stmt, "s", $param_email);
 			
-			
-			
 			// Set parameters
-			
 			$param_email = $email;
 			
-			
-			
 			// Attempt to execute the prepared statement
-			
 			if(mysqli_stmt_execute($stmt)){
 				
 				// Store result
-				
 				mysqli_stmt_store_result($stmt);
 				
-				
-				
 				// Check if email exists, if yes then verify password
-				
 				if(mysqli_stmt_num_rows($stmt) == 1){
 					
 					// Bind result variables
-					
 					mysqli_stmt_bind_result($stmt, $email, $hashed_password);
 					
 					if(mysqli_stmt_fetch($stmt)){
-						
 						if(password_verify($password, $hashed_password)){
 							
 							/* Password is correct, so start a new session and
-							
 							save the username to the session */
-							
-							session_start();
-							
+							session_start();	
 							$_SESSION['email'] = $email;
-							
 							header("location: welcome.php");
 							
 						} else{
-							
 							// Display an error message if password is not valid
-							
 							$password_err = 'The password you entered was not valid.';
-							
 						}
-						
 					}
 					
 				} else{
-					
 					// Display an error message if email doesn't exist
-					
 					$email_err = 'No account found with that email.';
-					
 				}
 				
 			} else{
-				
 				echo "Oops! Something went wrong. Please try again later.";
-				
 			}
-			
 		}
 		
-		
-		
 		// Close statement
-		
 		mysqli_stmt_close($stmt);
 		
 	}
 	
-	
-	
 	// Close connection
-	
 	mysqli_close($link);
-	
 }
-
 ?>
+
 <body>
 	<div class="header">
 		<div class="container">
